b0be63004d458e76156f6440a11ed7e26259e20d
Kazuya Takei <myself@attakei.net>
2025-02-02 13:41:40 +0900

    reafactor: Set local

--- root/.config/wezterm/wezterm.lua
+++ root/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-function file_exists(name)
+local function file_exists(name)
     local f = io.open(name, "r")
     return f ~= nil and io.close(f)
 end
