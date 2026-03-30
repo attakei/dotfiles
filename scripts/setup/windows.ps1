6041598c6d7ace5d72a7dc4852f2fb09af19ca1b
Kazuya Takei <myself@attakei.net>
2026-03-30 22:24:09 +0900

    feat(scoop): For idempotent

--- scripts/setup/windows.ps1
+++ scripts/setup/windows.ps1
@@ -1,8 +1,22 @@
 # ================================
 # Entrypoint for set up Windows OS
 # ================================
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 # Scoop
 # =====
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+# Install
+# -------
+$env:PATH = "$env:USERPROFILE\scoop\shims;$env:PATH"
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
+
+# Configuration
+# -------------
+scoop config aria2-warning-enabled false
+
+# Import current items
+# --------------------
+scoop import files/scoop-lock.json
