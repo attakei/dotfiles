2ca6ca824e4da3133f6ec51fd55154724453a978
Kazuya Takei <myself@attakei.net>
2026-04-06 20:04:54 +0900

    fix(nvim): Shorten cache dir for luac

--- app/NeoVim/init.lua
+++ app/NeoVim/init.lua
@@ -1,3 +1,9 @@
+if vim.fn.has "win32" == 1 then
+  local luac_dir = vim.fn.expand "~/.cache" .. "/nvim/luac"
+  vim.fn.mkdir(luac_dir, "p")
+  vim.loader.path = luac_dir
+end
+
 -- Fetch lazy.nvim if it is not exists.
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
