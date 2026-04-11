54221e5dbc0e8311be74b7fbec0fc5ef3c10ee8b
Kazuya Takei <myself@attakei.net>
2026-04-12 02:49:45 +0900

    fix(neovim): Custom lspconfigs disable install by mason

--- app/NeoVim/lua/mylsp/nu.lua
+++ app/NeoVim/lua/mylsp/nu.lua
@@ -1,4 +1,5 @@
 return {
+  mason = false,
   cmd = function(dispatchers, config)
     local bin_path = vim.fn.trim(vim.fn.system('aqua which nu'))
     return vim.lsp.rpc.start({ bin_path, '--lsp' }, dispatchers, { cwd = config.root_dir })
--- app/NeoVim/lua/mylsp/ruff.lua
+++ app/NeoVim/lua/mylsp/ruff.lua
@@ -1,4 +1,5 @@
 return {
+  mason = false,
   cmd = function(dispatchers, config)
     local uv_bin = vim.fn.trim(vim.fn.system('aqua which uv'))
     return vim.lsp.rpc.start(
